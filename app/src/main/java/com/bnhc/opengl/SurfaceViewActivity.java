bd3082986acbe08c910e3312163e5b2725720c0a
博纳海川 <bnhc@foxmail.com>
2018-05-05 10:29:40 +0800

    Add SurfaceView

--- app/src/main/java/com/bnhc/opengl/SurfaceViewActivity.java
+++ app/src/main/java/com/bnhc/opengl/SurfaceViewActivity.java
@@ -1,8 +1,11 @@
 package com.bnhc.opengl;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 /**
  * Created by bnhc on 18-5-5.
@@ -15,5 +18,33 @@ public class SurfaceViewActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+
+    private class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+        public MySurfaceView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
