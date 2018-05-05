a64be40e085aa6973557d3de5b9d0f6a1e6d9d5a
博纳海川 <bnhc@foxmail.com>
2018-05-05 10:25:39 +0800

    Add SurfaceView

--- app/src/main/java/com/bnhc/opengl/SurfaceViewActivity.java
+++ app/src/main/java/com/bnhc/opengl/SurfaceViewActivity.java
@@ -1,8 +1,19 @@
 package com.bnhc.opengl;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by bnhc on 18-5-5.
+ * SurfaceView
  */
 
-public class SurfaceViewActivity {
+public class SurfaceViewActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
